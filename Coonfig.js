a2f0284887fc73c3ddfad55f7a075ac6373a24b8
Aruj-dewangan <75670382+Aruj-dewangan@users.noreply.github.com>
2022-04-16 04:28:46 -0700

    Add files via upload

---
+++ Coonfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBrRVgSDfHtTbLkXUgjOYRRUaQarzQxuLY",
+  authDomain: "astro-science-facts-d1aad.firebaseapp.com",
+  projectId: "astro-science-facts-d1aad",
+  storageBucket: "astro-science-facts-d1aad.appspot.com",
+  messagingSenderId: "373976411085",
+  appId: "1:373976411085:web:21549a6b1ba42a921fb284"
+};
